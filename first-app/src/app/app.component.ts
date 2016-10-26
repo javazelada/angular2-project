22d22c79ac4ed310fc82de6a2321a75a1b4b6942
javaz zelada <javazelada@gmail.com>
2016-10-25 21:00:46 -0400

    onchange

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -3,14 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <fa-lifecycle *ngIf="!delete" >
+    <h1>root Component</h1>
+    <fa-lifecycle *ngIf="!delete" [bindable]="boundValue" >
     	<p>{{test}}</p>
-    </fa-lifecycle> 
+    </fa-lifecycle>
     <button (click)="delete = true" >click to delete</button>
+    <button (click)="test = 'Change value'" >click to Change content</button>
+    <button (click)="boundValue = 2000" >click to Change binding</button>
     `
 })
 export class AppComponent {
     delete = false;
     test = 'start value';
+    boundValue = 1000;
 }
- 
--- first-app/src/app/lifecycle.component.ts
+++ first-app/src/app/lifecycle.component.ts
@@ -1,22 +1,27 @@
-import { Component, 
-  OnInit, 
-  OnChanges, 
-  DoCheck, 
-  AfterContentInit, 
-  AfterContentChecked, 
+import { Component,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
   AfterViewInit,
-  AfterViewChecked, 
-  OnDestroy 
+  AfterViewChecked,
+  OnDestroy,
+  Input
 } from '@angular/core';
 
 @Component({
   selector: 'fa-lifecycle',
   template: `
+    <ng-content></ng-content>
+    <hr>
+    <p>{{bindable}}</p>
   `,
   styles: []
 })
 export class LifecycleComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
+  @Input() bindable = 1000;
   constructor(){
   }
   ngOnChanges(){
@@ -29,17 +34,17 @@ export class LifecycleComponent implements OnChanges, OnInit, DoCheck, AfterCont
     this.log('ngDoCheck');
   }
   ngAfterContentInit(){
-    this.log('ngAfterContentInit');  
+    this.log('ngAfterContentInit');
   }
   ngAfterContentChecked(){
     this.log('ngAfterContentChecked');
-  } 
+  }
   ngAfterViewInit(){
     this.log('ngAfterViewInit');
-  } 
+  }
   ngAfterViewChecked(){
     this.log('ngAfterViewChecked');
-  } 
+  }
   ngOnDestroy(){
     this.log('ngOnDestroy');
   }

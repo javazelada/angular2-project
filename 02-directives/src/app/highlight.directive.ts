6bf6ffecb739268de0bbddc888453fd1d1093b63
javaz zelada <javazelada@gmail.com>
2016-11-03 22:31:46 -0400

    02-directives

---
+++ 02-directives/src/app/highlight.directive.ts
@@ -0,0 +1,30 @@
+import { Directive,HostListener,HostBinding, Input, OnInit} from '@angular/core';
+
+@Directive({
+  selector: '[dirHighlight]'
+})
+export class HighlightDirective {
+
+  @HostListener('mouseenter') mouseover(){
+    this.backgroundColor = this.highlightColor;
+  };
+  @HostListener('mouseleave') onmouseleave(){
+    this.backgroundColor = this.defaultColor;
+  };
+  @HostBinding('style.backgroundColor') get setColor(){
+    return this.backgroundColor;
+  };
+  @Input() defaultColor = 'white';
+  @Input('dirHighlight') highlightColor = 'blue';
+  private backgroundColor: string;
+  constructor() {
+    //this.elementRef.nativeElement.style.backgroundColor  = 'blue';
+    //this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color','green');
+
+  }
+  ngOnInit(){
+    this.backgroundColor = this.defaultColor;
+  }
+
+
+}
